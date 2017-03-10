fdc505b3418cb982405a89d94281f26437e8469a
Klemen Košir <klemen.kosir@kream.io>
2017-03-10 18:36:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-momentjs
+A tiny Moment.js wrapper for Vue.
+
+### Installation
+```bash
+yarn add moment vue-momentjs
+```
+
+```javascript
+import Vue from "vue";
+import moment from "moment";
+import VueMomentJS from "vue-momentjs";
+
+Vue.use(VueMomentJS, moment);
+```
+
+### Using
+Call `this.$moment()` in any component.
---
+++ index.js
@@ -0,0 +1,24 @@
+(function() {
+	function plugin(Vue, moment) {
+		if (!moment) {
+			console.error("Please install Moment.js");
+			return;
+		}
+
+		Vue.moment = moment;
+
+		Object.defineProperties(Vue.prototype, {
+			$moment: {
+				get() {
+					return moment;
+				}
+			},
+		});
+	}
+
+	if (typeof exports === "object") {
+		module.exports = plugin;
+	} else if(window.Vue && window.moment) {
+		Vue.use(plugin, window.moment);
+	}
+})();

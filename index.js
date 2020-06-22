f79051b7f7035d21cf00efe3b18d93198540bd82
Klemen Košir <klemen.kosir@kream.io>
2020-06-22 12:53:07 +0900

    Add .editorconfig

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 (function() {
-	function plugin(Vue, moment) {
-		if (!moment) {
-			console.error("Please install Moment.js");
-			return;
-		}
+  function plugin(Vue, moment) {
+    if (!moment) {
+      console.error("Please install Moment.js");
+      return;
+    }
 
-		Vue.moment = moment;
+    Vue.moment = moment;
 
-		Object.defineProperties(Vue.prototype, {
-			$moment: {
-				get: function() {
-					return moment;
-				}
-			},
-		});
-	}
+    Object.defineProperties(Vue.prototype, {
+      $moment: {
+        get: function() {
+          return moment;
+        }
+      },
+    });
+  }
 
-	if (typeof exports === "object") {
-		module.exports = plugin;
-	} else if (window.Vue && window.moment) {
-		Vue.use(plugin, window.moment);
-	}
+  if (typeof exports === "object") {
+    module.exports = plugin;
+  } else if (window.Vue && window.moment) {
+    Vue.use(plugin, window.moment);
+  }
 })();
